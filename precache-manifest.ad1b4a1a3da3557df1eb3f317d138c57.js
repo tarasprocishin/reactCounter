f202a732447e4d23bd0f162960b47d3456f0c29c
tarasprocishin <taras1procishin@gmail.com>
2019-07-30 11:30:19 +0300

    Updates

---
+++ precache-manifest.ad1b4a1a3da3557df1eb3f317d138c57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23965d8c620cc6d451eba76b4b5393e9",
+    "url": "/react_counter/index.html"
+  },
+  {
+    "revision": "71d628540ebfd535efd2",
+    "url": "/react_counter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "71d628540ebfd535efd2",
+    "url": "/react_counter/static/js/main.d8b9f243.chunk.js"
+  },
+  {
+    "revision": "c1d1916f1c04cd84be27",
+    "url": "/react_counter/static/js/runtime~main.d325259d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
